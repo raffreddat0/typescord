e0ee9ac424c7503dab2e927fc4fccaa83f09cf0a
raffreddat0 <giovannigpgepic@gmail.com>
2025-06-07 16:55:23 +0200

    new event and license update

---
+++ src/events/guildDelete.ts
@@ -0,0 +1,10 @@
+import { APIGuild } from "discord-api-types/v10";
+import Client from "@structures/client/main";
+import Guild from "@structures/guild";
+
+export default function guildCreate(client: Client, data: APIGuild) {
+    const guild = new Guild(client, data);
+    client.guilds.delete(guild.id);
+
+    client.emit("guildDelete", guild);
+}
